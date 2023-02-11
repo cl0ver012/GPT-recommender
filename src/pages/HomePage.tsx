4ad7c1411f4ac969eae2f7c15ef031f7cbc32e14
cl0ver012 <goodcristina2023@gmail.com>
2023-02-11 15:16:40 +0100

    Add React project layout

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage: React.FC = () => {
+    return (
+        <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <h1 style={{ fontSize: '4em' }}>GPT Recommender</h1>
+        </div>
+    )
+}
+
+export default HomePage
