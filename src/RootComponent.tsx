51497ca8763dbda7be902248a484d8b0af116038
cl0ver012 <goodcristina2023@gmail.com>
2023-02-15 16:03:18 +0100

    Optimizations and functions documentation

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFoundPage'
-import { ROUTES } from './resources/routes-constants'
 import './styles/main.sass'
 
 const RootComponent: React.FC = () => {
@@ -10,7 +9,7 @@ const RootComponent: React.FC = () => {
         <Router>
             <Routes>
                 <Route path="*" element={<NotFoundPage />} />
-                <Route path={ROUTES.HOMEPAGE_ROUTE} element={<HomePage />} />
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </Router>
     )
--- src/resources/routes-constants.ts
+++
@@ -1,3 +0,0 @@
-export const ROUTES = {
-    HOMEPAGE_ROUTE: '/'
-}
